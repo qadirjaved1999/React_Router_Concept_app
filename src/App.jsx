8b69c589cfcc98f2c2270bdab95725f642c19e6e
Qadir Javed <108120593+qadirjaved1999@users.noreply.github.com>
2024-08-23 18:39:49 +0500

    How to use React Router Dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './Components/Header/header'
+import Footer from './Components/Footer/Footer'
+import Home from './Components/Home/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      Hy Qadir How are you?
+      {/* <Header />
+      <Home />
+      <Footer /> */}
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,46 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+import Layout from './Components/Layout.jsx';
+import Home from './Components/Home/Home.jsx';
+import About from './Components/About/About.jsx';
+import Contact from './Components/Contact/Contact.jsx';
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Layout />,
+//     children: [
+//       {
+//         path: '',
+//         element: <Home />
+//       },
+//       {
+//         path: 'about',
+//         element: <About />
+//       },
+//       {
+//         path: 'contact',
+//         element: <Contact />
+//       },
+//     ]
+//   }
+// ]);
+
+const route = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+      <Route path='about' element={<About />}/>
+      <Route path='contact' element={<Contact />}/>
+     
+    </Route>
+  )
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
+    {/* <App /> */}
   </StrictMode>,
 )
